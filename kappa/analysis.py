0b59c6110eeda4f1417ef1d30cdee1c571da0d14
Magda <magdalenar@onet.com.pl>
2022-02-03 12:26:22 +0100

    check values

--- kappa/analysis.py
+++ kappa/analysis.py
@@ -3,19 +3,35 @@ import pandas as pd
 #scikit-learn package
 from sklearn.metrics import cohen_kappa_score
 
+def check_v(val):
+    if len(set(val.values)) > 3:
+        print("Uwaga, kolumna", val.name, "zawiera więcej niż 3 wartości:", list(val.index))
+        case = input("Czy pokazać ich liczebności? (T/N): ").upper()
+        if case == "T":
+            print(val)
+
+
 def kappa(data, col1, col2):
     with open(data, encoding="utf8") as f:
         file = pd.read_csv(f, sep='\t', header=0, quoting=csv.QUOTE_NONE)
+        #sprawdzenie wartości
+        val1 = file[col1].value_counts()
+        val2 = file[col2].value_counts()
+        check_v(val1)
+        check_v(val2)
+
         ann1 = file[col1].to_list()
         ann2 = file[col2].to_list()
         result = cohen_kappa_score(ann1, ann2)
         return round(result, 4)
+
 
 def precision(data, col, decision):
     #decision: jakie decyzje liczyć jako trafne - "Y" czy "NA"
     with open(data, encoding="utf8") as f:
         file = pd.read_csv(f, sep='\t', header=0, quoting=csv.QUOTE_NONE)
         vals = file[col].value_counts()  # count values in column
+        check_v(vals)
         discard = vals["?"] if "?" in vals else 0  # number of '?' decisions to remove
         accurate = 0
         if decision == "NA":
