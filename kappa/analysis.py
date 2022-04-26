3056a6dc0fcb360f8ec6ef6b31e4f12dbcd48d28
Magda <magdalenar@onet.com.pl>
2022-04-26 19:10:46 +0200

    kappa function and strip()

--- kappa/analysis.py
+++ kappa/analysis.py
@@ -3,6 +3,32 @@ import pandas as pd
 #scikit-learn package
 from sklearn.metrics import cohen_kappa_score
 
+def cohen_kappa(ann1, ann2):
+    """Computes Cohen kappa for pair-wise annotators.
+    :param ann1: annotations provided by first annotator
+    :type ann1: list
+    :param ann2: annotations provided by second annotator
+    :type ann2: list
+    :rtype: float
+    :return: Cohen kappa statistic
+    """
+
+    count = 0
+    for an1, an2 in zip(ann1, ann2):
+        if an1 == an2:
+            count += 1
+    A = count / len(ann1)  # observed agreement A (Po)
+    uniq = set(ann1 + ann2)
+    print(uniq)
+    E = 0  # expected agreement E (Pe)
+    for item in uniq:
+        cnt1 = ann1.count(item)
+        cnt2 = ann2.count(item)
+        count = ((cnt1 / len(ann1)) * (cnt2 / len(ann2)))
+        E += count
+    return round((A - E) / (1 - E), 4)
+
+
 def check_v(val):
     if len(set(val.values)) > 3:
         print("Uwaga, kolumna", val.name, "zawiera więcej niż 3 wartości:", list(val.index))
@@ -22,7 +48,8 @@ def kappa(data, col1, col2):
 
         ann1 = file[col1].to_list()
         ann2 = file[col2].to_list()
-        result = cohen_kappa_score(ann1, ann2)
+        result = cohen_kappa(ann1, ann2) #ta funkcja bierze pod uwagę wartości nan
+        #result = cohen_kappa_score(ann1, ann2)
         return round(result, 4)
 
 
@@ -30,12 +57,15 @@ def precision(data, col, decision):
     #decision: jakie decyzje liczyć jako trafne - "Y" czy "NA"
     with open(data, encoding="utf8") as f:
         file = pd.read_csv(f, sep='\t', header=0, quoting=csv.QUOTE_NONE)
+        #normalizacja wyników w tej kolumnie (zwłaszcza dla drugiego pliku)
+        for x in file[col]:
+            file[col].replace({x: str(x).upper().strip()}, inplace=True)
         vals = file[col].value_counts()  # count values in column
         check_v(vals)
         discard = vals["?"] if "?" in vals else 0  # number of '?' decisions to remove
         accurate = 0
         if decision == "NA":
-            accurate = file[col].isna().sum() # number of NaN decisions
+            accurate = vals["NAN"] if "NAN" in vals else 0  # number of NaN decisions
         elif decision == "Y":
             accurate = vals["Y"] if "Y" in vals else 0  # number of 'Y' decisions
         if accurate != 0:
